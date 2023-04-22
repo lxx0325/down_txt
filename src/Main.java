3aa00c69adace069193d873ff31080134de5f3ee
lxx <598753711@qq.com>
2023-04-22 11:35:50 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import engine.FastDownloader;
+import source.E8;
+
+import java.io.IOException;
+
+/**
+ * Created By zia on 2018/10/5.
+ * 写了四个网站的解析，可能以后会添加更多
+ * 使用IFastDownloader抽象类进行统一调用
+ * 可以通过实现抽象类快速添加新的网站源解析
+ */
+public class Main {
+
+    //这两个参数请自行更换
+    private final static String bookName = "我的救世游戏成真了";
+    //存放目录，该目录是mac系统下的，windows需要自行更正“\\”
+    private final static String savePath = "G:\\down";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        FastDownloader downloader = new E8(bookName, "https://m.bqg116.com/html/76452/list.html", savePath);
+
+        //下载全部内容到一个txt文件里
+        downloader.downloadTXT();
+        //下载epub格式，自动生成索引
+//        downloader.downloadEPUB();
+        //下载epub并转换为mobi格式
+        //downloader.downloadMOBI();
+    }
+}
---
+++ src/source/E8.java
@@ -0,0 +1,62 @@
+package source;
+
+import bean.Chapter;
+import bean.ChapterBuffer;
+import engine.FastDownloader;
+import util.RegexUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created By byakuya on 2018/10/5.
+ * E8中文  http://www.e8zw.com
+ * 测试约满速
+ */
+public class E8 extends FastDownloader {
+
+    public E8(String bookName, String catalogUrl, String path) {
+        super(bookName, catalogUrl, path);
+    }
+
+    @Override
+    public List<Chapter> getChapters(String catalogUrl) throws IOException {
+        String html = getHtml(catalogUrl, "utf-8");
+        String first = RegexUtil.regexExcept("<div class=\"section-box\">", "<script>", html).get(0);
+        String dirtys = RegexUtil.regexInclude("<ul class=\"section-list fix\">", "</div>", first).get(1);//删除最新章节
+        String ddHtml = first.substring(dirtys.length()-400);
+        List<String> DDs = RegexUtil.regexExcept("<li>", "</li>", ddHtml);
+        List<Chapter> chapters = new ArrayList<>();
+        for (String dd : DDs) {
+            String href = catalogUrl + RegexUtil.regexExcept("href=\"", "\">", dd).get(0);
+            String name = RegexUtil.regexExcept(">", "<", dd).get(0);
+            Chapter chapter = new Chapter();
+            chapter.setHref(href);
+            chapter.setName(name);
+            chapters.add(chapter);
+        }
+        return chapters;
+    }
+
+    @Override
+    public ChapterBuffer adaptBookBuffer(Chapter chapter, int num) throws IOException {
+        String html = getHtml(chapter.href, "utf-8");
+        String contents = RegexUtil.regexExcept("<div class=\"posterror\">", "<div class=\"abottom\">", html).get(0);
+        String texts[] = contents.split("<br>|<br/>");
+
+        ChapterBuffer buffer = new ChapterBuffer();
+        List<String> lines = new ArrayList<>();
+        for (String text : texts) {
+            if (!text.trim().isEmpty()) {
+                lines.add(cleanContent(text));
+            }
+        }
+
+        buffer.content = lines;
+        buffer.name = chapter.name;
+        buffer.number = num;
+        return buffer;
+    }
+}
